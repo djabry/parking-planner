dd63d6260870331afacfb412a3a49705ab6eeb07
djabry <djabry@gmail.com>
2014-02-17 13:00:15 +0000

    Updated db connection resource names and all output types to include json and xml

--- parking-api/src/main/java/org/datahack/parking/api/service/BayFacadeREST.java
+++ parking-api/src/main/java/org/datahack/parking/api/service/BayFacadeREST.java
@@ -64,7 +64,7 @@ public class BayFacadeREST extends AbstractFacade<Bay> {
 
     @GET
     @Override
-    @Produces({"application/json"})
+    @Produces({"application/xml", "application/json"})
     public List<Bay> findAll() {
         return super.findAll();
     }
